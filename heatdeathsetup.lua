7a2c9a77425abf5cbb0d33b6315473873d98503c
HeatDeaths <deathheat005@gmail.com>
2025-05-28 21:27:47 -0600

    Update heatdeathsetup.lua

--- heatdeathsetup.lua
+++ heatdeathsetup.lua
@@ -72,6 +72,7 @@ for i, element in pairs(SF1E) do
 				TB2.MouseButton1Click:Connect(function()
 					if vvvv.Func ~= nil then
 						print(vvvv.Func)
+						loadstring(vvvv.Func)
 					end
 				end)
 			end
